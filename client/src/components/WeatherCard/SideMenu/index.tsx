553ecf78b350bf0c1ff63594a42ee762c11741ea
Andre Sch <andresch.dev@gmail.com>
2023-01-05 14:24:33 -0300

    Abstract Side Menu logic inside a container

--- client/src/components/WeatherCard/SideMenu/index.tsx
+++ client/src/components/WeatherCard/SideMenu/index.tsx
@@ -1,7 +1,4 @@
-import { useContext } from "react"
-
-import { MenuDisplayGetterContext, MenuDisplaySetterContext } from "../../../MenuDisplayProvider"
-
+import { SideMenuContainer } from "./container"
 import { SideMenuHeader } from "./SideMenuHeader"
 import { CityInput } from "./CityInput"
 import { CityList } from "./CityList"
@@ -9,20 +6,11 @@ import { CityList } from "./CityList"
 import "./index.css"
 
 export function SideMenu() {
-  const isMenuOpen = useContext(MenuDisplayGetterContext)
-  const setIsMenuOpen = useContext(MenuDisplaySetterContext)
-
   return (
-    <div className={`side-menu ${isMenuOpen ? 'show' : ''}`}>
-      <div className="content">
-        <SideMenuHeader />
-        <CityInput />
-        <CityList />
-      </div>
-      <div
-        className={`dark-overlay ${isMenuOpen ? 'show' : ''}`}
-        onClick={() => setIsMenuOpen(false)}>
-      </div>
-    </div>
+    <SideMenuContainer>
+      <SideMenuHeader />
+      <CityInput />
+      <CityList />
+    </SideMenuContainer>
   )
 }
