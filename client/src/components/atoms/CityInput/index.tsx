d34d452259a8748dae99731dcc0485eb082bac5c
Andre Sch <andresch.dev@gmail.com>
2023-01-22 20:42:19 -0300

    Decouple CityInput from parent component

--- client/src/components/atoms/CityInput/index.tsx
+++ client/src/components/atoms/CityInput/index.tsx
@@ -2,7 +2,7 @@ import "./index.css"
 
 export function CityInput() {
   return (
-    <div className="input-container">
+    <div className="city-input-container">
       <input type="text" placeholder="Add a new city"/>
       <img src="/assets/other/add-city.svg" alt="" draggable={false} />
     </div>
